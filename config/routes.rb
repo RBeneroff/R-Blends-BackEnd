01c0b2bfeba37c04572baa43adb6f9f2de2f8152
Rachel Beneroff <rbbeneroff@gmail.com>
2016-11-10 17:38:21 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create, :show, :destroy, :index, :update] do
+    resources :color_schemes, only: [:create, :destroy, :index, :update, :show]
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
+
+
 end
