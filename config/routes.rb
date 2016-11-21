bb02e8655802874f35a92b28d24df913cd44da65
Rachel Beneroff <rbbeneroff@gmail.com>
2016-11-21 10:59:03 -0500

    current user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   resources :users, only: [:create, :show, :destroy, :index, :update] do
     resources :color_schemes, only: [:create, :destroy, :index, :update, :show]  do
     end
     collection do
       post '/login', to: 'users#login'
+      get '/current-user', to: 'users#current'
       delete '/:user_id/color_schemes', to: 'color_schemes#destroy_all'
 
     end
